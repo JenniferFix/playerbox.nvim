a00dd1dc394e4e1b5079872a07478c866afb6241
Jennifer Fix <codingjennifer@gmail.com>
2024-10-02 21:28:11 -0600

    more of same fumbling

--- lua/playerbox/sonos/avtransport.lua
+++ lua/playerbox/sonos/avtransport.lua
@@ -1,5 +1,18 @@
-local AVTransport = require("playerbox.sonos.base")
+local AVTransport = {}
+local base = require("playerbox.sonos.base")
 AVTransport.__index = AVTransport
+
+function AVTransport:new(opts)
+	opts = {
+		name = "AVTransport",
+		ip = opts.ip,
+		port = opts.port or 1400,
+		controlUrl = "/MediaRenderer/AVTransport/Control",
+		eventSubUrl = "/MediaRenderer/AVTransport/Event",
+		SCPDURL = "/xml/AVTransport1.xml",
+	}
+	setmetatable(opts, base:new(opts))
+end
 
 function AVTransport:Play()
 	self:request()
